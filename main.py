50a219c8216b41a6d92d48b3279d04d228c88da7
ivanmilangaenoyer <122828493+Matapollos7000@users.noreply.github.com>
2024-06-06 11:29:35 -0300

    actualización

---
+++ main.py
@@ -0,0 +1,91 @@
+# Se dispone de un archivo con datos acerca de películas, que tiene el siguiente formato:
+# id_peli, titulo, genero, rating
+# por ejemplo: 1,Adventures of Rocky,sin genero,0
+# 2,My Brother the Devil,sin genero,0
+# 3,Criminal,sin genero,0
+# Se deberá realizar un programa que permita el análisis de dicho archivo y sea capaz de generar
+# nuevos archivos de salida de formato similar filtrados por varios criterios:
+# el programa contará con el siguiente menú:
+# 1) Cargar archivo .CSV: Se pedirá el nombre del archivo y se cargará en una lista de
+# diccionarios los elementos
+# del mismo.
+# 2) Imprimir lista: Se imprimirá por pantalla la tabla con los datos de las películas.
+# 3) Asignar rating: Se deberá hacer uso de la función map. La cual recibirá la lista y una
+# función que asignará a la película un valor de rating flotante entre 1 y 10 con 1 decimal
+# calculado de manera aleatoria se mostrará por pantalla el mismo.
+# 4) Asignar género: Se deberá hacer uso de la función map. La cual recibirá la lista y una
+# función que asignará a la película un género de acuerdo a un número aleatorio entre 1 y 4.
+# 1: drama
+# 2: comedia
+# 3: acción
+# 4: terror
+# 5) Filtrar por género: Se deberá pedir un género y escribir un archivo igual al original, pero
+# donde solo aparezcan películas del género seleccionado. El nombre del archivo será p.e. comedias.csv
+# 6) Ordenar películas: Se deberá mostrar por pantalla un listado de las películas ordenadas por
+# género y dentro de las del mismo género que aparezcan ordenadas por rating descendente.
+# 7) Informar Mejor Rating: Mostrar el titulo y el rating de la película con más rating
+# 8) Guardar películas: Se deberá guardar el listado del punto anterior en un archivo JSON.
+import os
+from modulo_funciones import *
+
+path_0 = os.getcwd()
+path = os.path.join(path_0, "Parcial-2024-Bauss-/movies.csv")
+
+programa = True
+menu_opciones = ("Que desea hacer?\n"
+                "1: Cargar CSV\n"
+                "2: Imprimir lista\n"
+                "3: Asignar rating\n"
+                "4: Asignar género\n"
+                "5: Filtrar por género\n"
+                "6: Ordenar películas\n"
+                "7: Informar Mejor Rating\n"
+                "8: Guardar películas\n"
+                "9: Salir\n")
+
+while programa:
+    
+    opcion = input(menu_opciones)
+    try:
+        opcion = int(opcion)
+    except:
+        raise ValueError("Por favor, ingrese numeros solamente")
+    
+    while opcion < 1 or opcion > 9:
+        opcion = input(f"Por favor ingrese numeros validos {menu_opciones}")
+
+    match opcion:
+        case 1:
+            #1
+            peliculas = cargar_csv(path)
+        case 2:
+            #2
+            mostrar_peliculas(peliculas)
+        case 3:
+            #3
+            mapear_pelicula(peliculas, asignar_rating)
+        case 4: 
+            #4
+            mapear_pelicula(peliculas, asignar_genero)
+        case 5:
+            #5
+            genero = input("Ingrese el genero que quieres filtrar: drama, comedia, acción, terror ")
+
+            while genero != "drama" and genero != "comedia" and genero != "acción" and genero != "terror":
+                genero = input("Rengrese el genero que quieres filtrar: drama, comedia, acción, terror ")
+
+            filtrar_genero(peliculas, genero, path_0)
+        case 6:
+            #6
+            ordenar_genero_rating_dsc(peliculas)
+        case 7:
+            #7
+            titulo_max, rt_max = rating_max(peliculas)
+            mostrar_peliculas(peliculas)
+            print(f"La pelicula con mayor rating es: {titulo_max} con: {rt_max}.pts de rating")
+        case 8:
+            #8
+            guardar_peliculas_json(peliculas, path_0)
+        case 9:
+            programa = False
+
---
+++ modulo_funciones.py
@@ -0,0 +1,189 @@
+import random, math, json
+
+def cargar_csv(path: str) -> list:
+    """Recibe una dirección de memoria, carga el csv en una lista 
+    de diccionarios y devuelve esa lista
+
+
+    Args:
+        path (str): Dirección de memoria
+
+    Returns:
+        Lista(list): Una lista de diccionarios
+    """
+    with open(path, "r", encoding = "utf-8") as archivo:
+        lista = []
+        encabezado = archivo.readline().strip("\n").split(",")
+        for linea in archivo.readlines():
+            peliculas = {}
+            linea = linea.strip("\n").split(",")
+            id, titulo, genero, rating = linea 
+            
+            peliculas["id"] = int(id)
+            peliculas["titulo"] = titulo
+            peliculas["genero"] = genero
+            peliculas["rating"] = int(rating)
+            lista.append(peliculas)
+
+        return lista
+    
+def mostrar_peliculas(peliculas: list):
+    """Recibe una lista de diccionarios y la muestra en pantalla. No devuelve nada
+
+    Args:
+        peliculas (list): Las peliculas a mostrar
+    """
+    print("ID           TITULO                       GENERO      RATING")
+    for i in range(len(peliculas)):
+        print(f" {peliculas[i]["id"]:3}    {peliculas[i]["titulo"]:<30}   {peliculas[i]["genero"]:<8}      {peliculas[i]["rating"]:2}")
+
+
+def mapear_pelicula(lista: list, funcion):
+    """Recibe una lista de diccionarios y una funcion.
+    Mapea la lista y no devuelve nada
+
+    Args:
+        lista (list): La lista a mapear
+        funcion (funct): La funcion que mapea la lista
+    """
+    for i in range(len(lista)):
+        funcion(lista, i)
+
+def asignar_rating(lista: list, pelicula: int):
+    """Recibe una pelicula y le asigna un numero del 1 al 10 y un decimal
+
+    Args:
+        lista (list): La lista a asignar
+        pelicula (int): La pelicula del valor que se va a cambiar
+    """
+    lista[pelicula]["rating"] = round(random.uniform(1, 10), 1)
+
+
+
+def asignar_genero(lista: list, pelicula: int):
+    """Recibe una generi y le asigna unode acuerdo a un número aleatorio entre 1 y 4.
+
+    Args:
+        lista (list): La lista a asignar
+        pelicula (str): La pelicula del genero que se va a cambiar
+    """
+    numero = random.randint(1, 4)
+    if numero == 1:
+        genero = "drama"
+    elif numero == 2:
+        genero = "comedia"
+    elif numero == 3:
+        genero = "acción"
+    elif numero == 4:
+        genero = "terror"
+
+    lista[pelicula]["genero"] = genero
+
+def filtrar_genero(lista:list, genero: str, path: str):
+    """Recibe una lista, un genero y una dirrecion de memoria, 
+    crea un archivo csv y guarda las peliculas del genero correspondiente
+
+    Args:
+        lista (list): La lista a escribir
+        genero (str): EL genero a filtrar
+        path (str): La dirrecion de memoria donde crear el archivo
+    """
+    with open(f"{path}/Parcial-2024-Bauss-/p.e.comedias.csv", "w", encoding = "utf-8") as archivo:
+        archivo.write("ID           TITULO                   GENERO      RATING\n")
+        for i in range(len(lista)):
+            if lista[i]["genero"] == genero:
+                id = f"{lista[i]["id"]:3}  "
+                genero = lista[i]["genero"]
+                titulo = f"  {lista[i]["titulo"]:<30} " 
+                rating = f"       {lista[i]["rating"]:2}\n"
+
+                archivo.write(id)
+                archivo.write(titulo)
+                archivo.write(genero)
+                archivo.write(rating)
+
+
+def rating_max(lista: list):
+    """Recibe una lista, calcula cual es la pelicula con el 
+    rating mas alta, devuelve el nombre y rating de esta
+
+    Args:
+        lista (list): La lista a comparar
+
+    Returns:
+        float: El titulo la pelicula
+        str: El rating de la pelicula
+    """
+    titulo_max = "Ninguno"
+    rating_max = 0
+    bandera = True
+    for i in range(len(lista)):
+        if lista[i]["rating"] > rating_max or bandera:
+            bandera = False
+            titulo_max = lista[i]["titulo"]
+            rating_max = lista[i]["rating"]
+
+    return titulo_max, rating_max
+
+def cambiar_peliculas(lista, i, j):
+    """Recibe una lista y dos peliculas,
+    las cambia de lugar, 
+    no devuelve nada
+
+    Args:
+        lista (_type_): La lista con las peliculas
+        i (int): Primer pelicula
+        j (int): Segunda pelicula
+    """
+    for clave, valor in lista[i].items():
+        aux = lista[i][clave]
+        lista[i][clave] = lista[j][clave]
+        lista[j][clave] = aux
+
+
+def ordenar_genero_rating_dsc(lista: list):
+    """Recibe una lista,
+    ordena la lista por genero dentro de este por rating descendiente
+    no devuelve nada
+
+    Args:
+        lista (list): La lista a ordenar
+    """
+    for i in range(len(lista) -1):
+        for j in range(i + 1, len(lista)):
+            if lista[i]["genero"] == lista[j]["genero"]:
+                if lista[i]["rating"] < lista[j]["rating"]:
+                    cambiar_peliculas(lista, i, j)
+            elif lista[i]["genero"] > lista[j]["genero"]:
+                cambiar_peliculas(lista, i, j)
+
+
+def guardar_peliculas_json(lista: list, path: str):
+    # with open(f"{path}/Parcial-2024-Bauss-/p.e.ordenadas.json", "w", encoding = "utf-8") as archivo:
+    #     archivo.write("ID           TITULO                  GENERO         RATING\n")
+    #     for i in range(len(lista)):
+    #             id = f"{lista[i]["id"]:3}  "
+    #             genero = lista[i]["genero"]
+    #             titulo = f"  {lista[i]["titulo"]:<30} " 
+    #             rating = f"     {lista[i]["rating"]:2}\n"
+
+    #             archivo.write(id)
+    #             archivo.write(titulo)
+    #             archivo.write(genero)
+    #             archivo.write(rating)
+
+    with open(f"{path}/Parcial-2024-Bauss-/p.e.ordenadas.json", "w", encoding = "utf-8") as archivo:
+        # for i in range(len(lista)):
+                # id = f"{lista[i]["id"]:3}  "
+                # genero = lista[i]["genero"]
+                # titulo = f"  {lista[i]["titulo"]:<30} " 
+                # rating = f"     {lista[i]["rating"]:2}\n"
+
+                # archivo.write(id)
+                # archivo.write(titulo)
+                # archivo.write(genero)
+                # archivo.write(rating)
+
+
+        json.dump(lista, archivo, ensure_ascii = False, indent = 4)
+
