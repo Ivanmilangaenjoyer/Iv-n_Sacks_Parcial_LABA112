a54b581afd9a9163773f526b9bf2e0651e2cd11c
Ivanmilangaenjoyer <ivas.sacks66@gmail.com>
2024-06-06 11:37:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import os
 from modulo_funciones import *
 
 path_0 = os.getcwd()
-path = os.path.join(path_0, "Parcial-2024-Bauss-/movies.csv")
+path = os.path.join(path_0, "Iv-n_Sacks_Parcial_LABA112movies.csv")
 
 programa = True
 menu_opciones = ("Que desea hacer?\n"
